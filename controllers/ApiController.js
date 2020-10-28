4f978232fec07b7437068781c92a7969c5c4804e
sonutiwari <ankorha@gmail.com>
2020-10-28 13:30:37 +0530

    Added env file to hide sensitive data

--- controllers/ApiController.js
+++ controllers/ApiController.js
@@ -1,4 +1,5 @@
 const fetch = require("node-fetch");
+require("dotenv").config();
 const URL = "https://bravenewcoin.p.rapidapi.com/market-cap";
 class ApiController {
   fetchDataFromUrl = async (req, res) => {
@@ -7,10 +8,9 @@ class ApiController {
     fetch(newUrl, {
       method: "GET",
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik5EVXhNRGhHT0VReE56STVOelJCTTBJM1FrUTVOa0l4TWtRd1FrSTJSalJFTVRaR1F6QTBOZyJ9.eyJpc3MiOiJodHRwczovL2F1dGguYnJhdmVuZXdjb2luLmNvbS8iLCJzdWIiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWUBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly9hcGkuYnJhdmVuZXdjb2luLmNvbSIsImlhdCI6MTYwMzc5NDAxNywiZXhwIjoxNjAzODgwNDE3LCJhenAiOiJvQ2RRb1pvSTk2RVJFOUhZM3NRN0ptYkFDZkJmNTVSWSIsInNjb3BlIjoicmVhZDppbmRleC10aWNrZXIgcmVhZDpyYW5raW5nIHJlYWQ6bXdhIHJlYWQ6Z3dhIHJlYWQ6YWdncmVnYXRlcyByZWFkOm1hcmtldCByZWFkOmFzc2V0IHJlYWQ6b2hsY3YgcmVhZDptYXJrZXQtY2FwIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.uAEiViTmEmQ4KoscUy197ErpgvM4dHP4MAhke9dONXCcMY_f5pSoqYjAEnv6fthLsz9blTlxmvgNVdF5uNA7iqNiBUCrwW391kEA44FFroLajxdOxeBOnxzRZYqt6SDHx-j2dICzACM4oV-84l2V3socEvVNqpmKQo2Gp9YZaQ31aYGfKnAigUuHyk5lIwaOH8iJ-2JgWChqG0BDZx_kVXWD58UIsXmyFtvnyJdcVjPQSLaNY94T8HIlMU4F5igRwdRTG3Ke1CWTJlH4cLZOXhromGwarw5VZyk8jtciVoA_oHM7IDXv8SwhiNiKnmwt4lS7codoQuO4a1lM_tvmdg",
+        Authorization: `Bearer ${process.env.BEARER}`,
         "x-rapidapi-host": "bravenewcoin.p.rapidapi.com",
-        "x-rapidapi-key": "bb3d0cb62cmsh72b5b1b3772b0bfp11bf9bjsn6162a034aaf7",
+        "x-rapidapi-key": process.env.API_KEY,
         // 'Content-Type': 'application/x-www-form-urlencoded',
       },
     })
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const rootRouter = require("./routers/rootRouter");
 const app = express();
-const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", rootRouter);
-app.listen(PORT, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (err) {
     console.error(err.message);
   } else {
-    console.log("App is running on PORT", PORT);
+    console.log("App is running on PORT", process.env.PORT);
   }
 });
